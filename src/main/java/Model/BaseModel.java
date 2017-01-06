db2dfa00db350e5778c085fc87b7901a5a606e5a
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2017-01-06 14:32:55 -0800

    added model package

---
+++ src/main/java/Model/BaseModel.java
@@ -0,0 +1,35 @@
+package Model;
+
+import structure.Author;
+import structure.Paper;
+
+import java.util.HashMap;
+
+public abstract class BaseModel {
+    public static final String TYPE = "ab";
+    public static final String NAME = "Abstract Model";
+    public static final double INVALID_VALUE = -Double.MAX_VALUE;
+    public final String authorId;
+    protected Author author;
+    protected HashMap<String, Integer> citeCountMap;
+
+    public BaseModel(Author author) {
+        this.authorId = author.id;
+        this.author = author;
+        this.citeCountMap = new HashMap<>();
+    }
+
+    public void train() {
+        for (Paper paper : this.author.papers) {
+            for (String refPaperId : paper.refPaperIds) {
+                if (!this.citeCountMap.containsKey(refPaperId)) {
+                    this.citeCountMap.put(refPaperId, 1);
+                } else {
+                    this.citeCountMap.put(refPaperId, this.citeCountMap.get(refPaperId) + 1);
+                }
+            }
+        }
+    }
+
+    public abstract double estimate(Paper paper);
+}
