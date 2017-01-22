c29ca90a0c6fefaad6ee5ba7684aec2500275925
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2017-01-22 12:34:59 -0800

    added RandomModel

---
+++ src/main/java/model/RandomModel.java
@@ -0,0 +1,25 @@
+package model;
+
+import structure.Author;
+import structure.Paper;
+
+import java.util.Random;
+
+public class RandomModel extends BaseModel {
+    public static final String TYPE = "rand";
+    public static final String NAME = "Random Model";
+    private final Random rand;
+
+    public RandomModel(Author author) {
+        super(author);
+        this.rand = new Random();
+    }
+
+    @Override
+    public void train() {}
+
+    @Override
+    public double estimate(Paper paper) {
+        return this.rand.nextDouble();
+    }
+}
