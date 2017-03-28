a7003ddebb1d48d22dcbc074b6a49c42b4f9d4ac
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2017-03-27 17:01:20 -0700

    fixed typos

--- src/main/java/structure/Paper.java
+++ src/main/java/structure/Paper.java
@@ -7,30 +7,30 @@ import java.util.HashSet;
 public class Paper {
     public final String id, year, venueId;
     public final String[] refPaperIds;
-    private final HashSet<String> authorSet;
+    private final HashSet<String> authorIdSet;
 
     public Paper(String inputLine) {
         String[] elements = inputLine.split(Config.FIRST_DELIMITER);
         this.id = elements[0];
         this.year = elements[1];
         this.venueId = elements[2];
-        this.authorSet = new HashSet<>();
+        this.authorIdSet = new HashSet<>();
         this.refPaperIds = elements[4].split(Config.SECOND_DELIMITER);
         String[] authorIds = elements[3].split(Config.SECOND_DELIMITER);
         for (String authorId : authorIds) {
-            this.authorSet.add(authorId);
+            this.authorIdSet.add(authorId);
         }
     }
 
     public HashSet<String> getAuthorSet() {
-        return this.authorSet;
+        return this.authorIdSet;
     }
 
     public int getAuthorSize() {
-        return this.authorSet.size();
+        return this.authorIdSet.size();
     }
 
     public boolean checkIfAuthor(String authorId) {
-        return this.authorSet.contains(authorId);
+        return this.authorIdSet.contains(authorId);
     }
 }
