0ac8aedb7e9cbffae32c9b5f4a2a7004c1b9f4a8
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2017-06-04 16:30:34 -0700

    renamed

--- src/main/java/model/CountUpModel.java
+++ src/main/java/model/CountUpModel.java
@@ -4,8 +4,8 @@ import structure.Author;
 import structure.Paper;
 
 public class CountUpModel extends BaseModel {
-    public static final String TYPE = "cu";
-    public static final String NAME = "Count Up Model";
+    public static final String TYPE = "cc";
+    public static final String NAME = "Common Citation Model";
 
     public CountUpModel(Author author) {
         super(author);
@@ -23,7 +23,7 @@ public class CountUpModel extends BaseModel {
     @Override
     public double estimate(Paper paper) {
         int[] counts = calcCounts(paper);
-        return counts[1] > 0 ? (double) counts[0] / (double) this.totalCitationCount : INVALID_VALUE;
+        return counts[1] > 0 ? (double) counts[0] : INVALID_VALUE;
     }
 
     public static boolean checkIfValid(String modelType) {
